5d130b574ac0f1420c38acd1f8006421854c5d2b
Receks <recek.se@gmail.com>
2018-01-08 11:43:18 +0100

    - Help Page - Different labels for the activities

---
+++ app/src/main/java/com/recek/huewakeup/app/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.recek.huewakeup.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.recek.huesleepwake.R;
+
+public class HelpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
--- app/src/main/java/com/recek/huewakeup/app/MainActivity.java
+++ app/src/main/java/com/recek/huewakeup/app/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -100,6 +103,30 @@ public class MainActivity extends Activity {
         });
 
         statusText = findViewById(R.id.statusText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_help:
+                startHelpActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void startHelpActivity() {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
     }
 
     private void updateSchedules() {
